43a9aa32fb021b7d10d8d1d07e4273d63cbe6714
majidisadev <majidisadev@gmail.com>
2025-12-17 08:52:12 +0700

    install-all

--- install-all.bat
+++ install-all.bat
@@ -3,5 +3,16 @@ cd /d "%~dp0"
 echo Installing all dependencies...
 echo.
 npm run install:all
+if %errorlevel% equ 0 (
+    echo.
+    echo ========================================
+    echo Instalasi berhasil!
+    echo ========================================
+) else (
+    echo.
+    echo ========================================
+    echo Instalasi gagal!
+    echo ========================================
+)
 pause
 
